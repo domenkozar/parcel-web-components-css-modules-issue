1c2522213dc40c38f19dae79bdeb4cc3f732c9ea
Domen Kožar <domen@dev.si>
2021-11-05 16:31:51 -0600

    initial minimal example demonstrating the issue

---
+++ README.md
@@ -0,0 +1,4 @@
+Parcel doesn't apply CSS module styles when using web components.
+
+    $ yarn install
+    $ yarn run parcel serve index.html
---
+++ app.jsx
@@ -0,0 +1,24 @@
+import { LazyLog, ScrollFollow } from 'react-lazylog';
+import ReactDOM from "react-dom";
+import React from "react";
+
+class LogViewer extends HTMLElement {
+    connectedCallback() {
+      this.mountPoint = document.createElement('div');
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.appendChild(this.mountPoint);
+
+      const url = this.getAttribute('url');
+
+      ReactDOM.render(
+        <ScrollFollow
+            startFollowing={true}
+            render={({ follow, onScroll }) => (
+              <LazyLog url={url} height={800} width={900} enableSearch stream follow={follow} onScroll={onScroll}></LazyLog> 
+            )}
+        />
+        , this.mountPoint);
+    }
+  }
+
+customElements.define('log-viewer', LogViewer);
